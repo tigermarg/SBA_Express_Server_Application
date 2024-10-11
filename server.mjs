93abe8f9041fcd6c5711066356bab4138f59f347
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-10-11 12:46:23 -0400

    CRUD requests

--- server.mjs
+++ server.mjs
@@ -8,6 +8,22 @@ let PORT = 3000;
 //Middleware
 
 //Routes
+app.get('/', (req, res) => {
+    res.send(`get`)
+})
+
+app.post('/', (req, res) => {
+    res.send(`post`)
+})
+
+app.patch('/', (req, res) => {
+    res.send(`patch`)
+})
+
+app.delete('/', (req, res) => {
+    res.send(`delete`)
+})
+
 
 //Listen
 app.listen(PORT, () => {
