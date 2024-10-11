8134d1b06c23a44e192c680943ea0ae369edb84c
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-10-11 12:28:51 -0400

    Initial Commit

---
+++ server.mjs
@@ -0,0 +1,15 @@
+//Import dependencies
+import express from 'express';
+
+//Create an instance of express
+const app = express();
+let PORT = 3000;
+
+//Middleware
+
+//Routes
+
+//Listen
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
